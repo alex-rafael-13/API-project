a3e484efac49c8b403e30ebd08b07cb24c80b134
Alex Rafael <texas@Alexs-MacBook-Air-2.local>
2022-12-19 00:31:32 -0800

    Added an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -19,4 +19,9 @@ router.get("/api/csrf/restore", (req, res) => {
   });
   // ...
 
+  //API router
+  const apiRouter = require('./api');
+
+  router.use('/api', apiRouter);
+
 module.exports = router;
