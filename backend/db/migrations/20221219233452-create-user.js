14d9e4a9615ca14bb337993501a35ff83c6474c2
Alex Rafael <texas@alexs-air-2.lan>
2022-12-20 17:09:13 -0800

    Added User signup backend endpoint

--- backend/db/migrations/20221219233452-create-user.js
+++ backend/db/migrations/20221219233452-create-user.js
@@ -12,10 +12,12 @@ module.exports = {
       username: {
         type: Sequelize.STRING(30),
         allowNull: false,
+        unique: true
       },
       email: {
         type: Sequelize.STRING(256),
-        allowNull: false
+        allowNull: false,
+        unique: true
       },
       hashedPassword: {
         type: Sequelize.STRING.BINARY,
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,23 @@
 const express = require('express')
+
+//Importing setToken, requireAuth, and User model
+const { setToken, requireAuth, setTokenCookie } = require('../../utils/auth')
+const { User } = require('../../db/models')
+
+//initiating router
 const router = express.Router()
 
+//POST Creating a new user
+router.post('/', async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({email, username, password});
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+        user: user
+    })
+})
 
 
 module.exports = router
